4d0dd303fbc0e793163ac482c4a6e5239e60ebf2
rohit-simbanic <>
2023-12-12 10:26:32 +0530

    font fixed

--- app/components/section-heading.tsx
+++ app/components/section-heading.tsx
@@ -9,9 +9,8 @@ export default function SectionHeading({ children }: SectionHeadingProps) {
   const pathname = usePathname();
   return (
     <h2
-      className={`text-${
-        pathname === "/service/cloud-engineering" ? "[40px]" : "[45px]"
-      } font-graphik font-bold leading-[124%] capitalize mb-8 text-[#111827]`}
+      className="text-[45px]
+      font-graphik font-bold leading-[124%] capitalize mb-8 text-[#111827]"
     >
       {children}
     </h2>

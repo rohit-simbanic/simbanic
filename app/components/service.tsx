ce94bbc9cab384d2c3fb33188fbd9c24299dcd59
rohit-simbanic <>
2023-12-20 12:44:39 +0530

    Product Engineering Layout Change [sc-1997]

--- app/components/service.tsx
+++ app/components/service.tsx
@@ -57,22 +57,52 @@ export default function Service() {
               className="w-full px-4 py-6 group transition-transform transform hover:scale-105 cursor-pointer"
               key={service.id}
             >
-              <div className="relative flex flex-col sm:flex-row gap-[36px] items-start group-hover:bg-[#0000001a] group-hover:bg-opacity-[0.1] group-hover:rounded-lg p-4 text-left">
-                <Image
-                  src={service.logoUrl}
-                  alt={service.name}
-                  width={70}
-                  height={70}
-                />
+              <div
+                className={`relative flex flex-col${
+                  pathname === "/service/product-engineering"
+                    ? ""
+                    : "sm:flex-row gap-[36px]"
+                } items-start group-hover:bg-[#0000001a] group-hover:bg-opacity-[0.1] group-hover:rounded-lg p-4 text-left`}
+              >
+                {pathname === "/service/product-engineering" ? (
+                  <>
+                    <div className="flex gap-2 w-full">
+                      <Image
+                        src={service.logoUrl}
+                        alt={service.name}
+                        width={70}
+                        height={70}
+                      />
 
-                <div>
-                  <h3 className="text-[25px] font-medium text-white leading-[134%] w-6 font-graphik dark:text-gray-100">
-                    {service.name}
-                  </h3>
-                  <p className="text-[14px] text-[#748CA3] w-3/4 my-5 font-prompt font-light leading-[134%] dark:text-gray-400">
-                    {service.description}
-                  </p>
-                </div>
+                      <div className="w-[91%">
+                        <h3 className="text-[25px] font-medium text-white leading-[134%] w-6 font-graphik dark:text-gray-100">
+                          {service.name}
+                        </h3>
+                      </div>
+                    </div>
+                    <p className="text-[14px] text-[#748CA3] my-5 font-prompt font-light leading-[134%] dark:text-gray-400">
+                      {service.description}
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <Image
+                      src={service.logoUrl}
+                      alt={service.name}
+                      width={70}
+                      height={70}
+                    />
+
+                    <div>
+                      <h3 className="text-[25px] font-medium text-white leading-[134%] w-6 font-graphik dark:text-gray-100">
+                        {service.name}
+                      </h3>
+                      <p className="text-[14px] text-[#748CA3] w-3/4 my-5 font-prompt font-light leading-[134%] dark:text-gray-400">
+                        {service.description}
+                      </p>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           ))}

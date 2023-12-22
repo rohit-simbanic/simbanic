2cd1e1e73eac30a8e5220a560391f392ac44cea9
rohit-simbanic <>
2023-12-22 13:31:49 +0530

    Quick Fix in Home Page [sc-2050]L

--- app/components/section-heading.tsx
+++ app/components/section-heading.tsx
@@ -9,8 +9,10 @@ export default function SectionHeading({ children }: SectionHeadingProps) {
   const pathname = usePathname();
   return (
     <h2
-      className="text-[45px]
-      font-graphik font-bold leading-[124%] capitalize mb-8 text-[#111827]"
+      className={`text-[45px]
+      font-graphik font-bold leading-[124%] capitalize mb-8 text-[#111827] ${
+        pathname === "/" ? "px-5" : ""
+      }`}
     >
       {children}
     </h2>

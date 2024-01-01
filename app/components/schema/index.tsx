9c6fd3ef5a352a6f85311b7f5283edc0f03e1cf7
rohit-simbanic <>
2024-01-01 17:27:40 +0530

    Form validation [sc-2102]

---
+++ app/components/schema/index.tsx
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+
+export const submitSchema = Yup.object({
+  name: Yup.string()
+    .matches(/^[a-zA-Z ]*$/, "Only characters are allowed")
+    .min(2)
+    .max(25)
+    .required("Please enter your name"),
+  company: Yup.string()
+    .matches(/^[a-zA-Z ]*$/, "Only characters are allowed")
+    .min(5)
+    .max(25)
+    .required("Please enter your company name"),
+  email: Yup.string().email().required("Please enter your email"),
+  phone: Yup.number()
+    .typeError("That doesn't look like a phone number")
+    .positive("A phone number can't start with a minus")
+    .integer("A phone number can't include a decimal point")
+    .min(8)
+    .required("A phone number is required"),
+  message: Yup.string().min(6).required("Please enter your message"),
+});
